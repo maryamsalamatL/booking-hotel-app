c3e4edffa5ac21a1f113d627a7adcf7010fc54fc
maryamsalamatl <maryamsalamat47@gmail.com>
2024-09-10 19:09:59 +0400

    fix: minor bugs

--- src/components/context/HotelsProvider.jsx
+++ src/components/context/HotelsProvider.jsx
@@ -8,8 +8,8 @@ export default function HotelsProvider({ children }) {
   const [searchParams] = useSearchParams();
   const destination = searchParams.get("destination");
   const options = JSON.parse(searchParams.get("options"));
-  const room = options.room;
-  const requiredBeds = options.adult + options.children;
+  const room = options?.room;
+  const requiredBeds = options?.adult + options?.children;
 
   const { isLoading, data: hotels } = useFetch(
     "http://localhost:5000/hotels",
