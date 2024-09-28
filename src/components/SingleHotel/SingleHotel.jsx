44b684cb2c219147e93ababa8e696c1f51c88f03
maryamsalamatl <maryamsalamat47@gmail.com>
2024-09-28 12:09:40 +0400

    feat: develop singleHotel component

--- src/components/SingleHotel/SingleHotel.jsx
+++ src/components/SingleHotel/SingleHotel.jsx
@@ -4,13 +4,31 @@ import useFetch from "../../hooks/useFetch";
 import Loader from "../Loader/Loader";
 import { useSetCurrentHotelId } from "../context/HotelsProvider";
 import { useEffect } from "react";
+import { MdLocationOn } from "react-icons/md";
 
 export default function SingleHotel() {
   const { id } = useParams();
   const { isLoading, data } = useFetch(
     `${process.env.REACT_APP_BASE_URL}/hotels/${id}`
   );
-  const { name, smart_location, xl_picture_url, number_of_reviews } = data;
+  const {
+    name,
+    smart_location,
+    xl_picture_url,
+    number_of_reviews,
+    description,
+    room_type,
+    accommodates,
+    bathrooms,
+    bedrooms,
+    beds,
+    bed_type,
+    house_rules,
+    interaction,
+    street,
+    minimum_nights,
+    maximum_nights,
+  } = data;
 
   const setCurrentHotelId = useSetCurrentHotelId();
 
@@ -29,6 +47,50 @@ export default function SingleHotel() {
             {number_of_reviews} &bull; {smart_location}
           </span>
           <img src={xl_picture_url} alt={name} />
+          <ul>
+            <p className={styles.details}>Details</p>
+            <li>
+              <span>Room type :</span> {room_type}
+            </li>
+            <li>
+              <span>Accommodates : </span>
+              {accommodates}
+            </li>
+            <li>
+              <span>Bathrooms :</span> {bathrooms}
+            </li>
+            <li>
+              <span>Bedrooms : </span>
+              {bedrooms}
+            </li>
+            <li>
+              <span>Beds : </span>
+              {beds}
+            </li>
+            <li>
+              <span>Bed type :</span> {bed_type}
+            </li>
+            <li>
+              <span>Minimum nights :</span> {minimum_nights}
+            </li>
+            <li>
+              <span>Maximum nights :</span> {maximum_nights}
+            </li>
+            <li>
+              <span>House rules :</span> {house_rules || "-"}
+            </li>
+          </ul>
+          <p className={styles.description}>
+            {description} ...
+            <br />
+            <br />
+            {interaction}
+          </p>
+          <p className={styles.address}>
+            <MdLocationOn />
+            <span>{street}</span>
+          </p>
+          <button className={`secondaryBtn ${styles.bookBtn}`}>Book Now</button>
         </>
       )}
     </div>
