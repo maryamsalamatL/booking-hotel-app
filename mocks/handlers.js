5dc24822b64fbdd7d9a829cae97e589f2237c447
maryamsalamatl <maryamsalamat47@gmail.com>
2024-12-13 20:50:46 +0400

    test(LocationList): refactor tests to fetch data from mock server

--- mocks/handlers.js
+++ mocks/handlers.js
@@ -1,12 +1,15 @@
 import { http, HttpResponse } from "msw";
-import { bookmarks } from "./data";
+import { bookmarks, hotels } from "./data";
 
 const BASE_URL = process.env.REACT_APP_BASE_URL;
 export const handlers = [
   http.get(`${BASE_URL}/bookmarks`, () => {
     return HttpResponse.json(bookmarks);
   }),
-  http.delete(`${BASE_URL}/bookmarks/:id`, (res) => {
+  http.delete(`${BASE_URL}/bookmarks/:id`, () => {
     return HttpResponse.json({ message: "successfully deleted" });
+  }),
+  http.get(`${BASE_URL}/hotels`, () => {
+    return HttpResponse.json(hotels);
   }),
 ];
--- src/components/LocationList/__test__/LocationList.cy.jsx
+++ src/components/LocationList/__test__/LocationList.cy.jsx
@@ -1,18 +1,23 @@
 import LocationList from "../LocationList";
+import { startMsw } from "../../../../mocks/server";
 
 describe("testing LocationList: ", () => {
+  before(() => {
+    startMsw();
+  });
+
   beforeEach(() => cy.mount(<LocationList />));
-  
+
   it("should render title correctly", () => {
     cy.contains("Nearby Locations").should("exist");
   });
 
-  it("should display loading when fetch items", () => {
+  it("should render loading, while data is loading", () => {
     cy.contains("Data is Loading ...").should("exist");
   });
 
-  it("should fetch location items", () => {
-    cy.getByDataCy("location-item-link").should("exist");
+  it("should render location items, when data loaded", () => {
+    cy.getByDataCy("location-item-link").should("have.length", 3);
   });
 
   // navigation tests with e2e test
